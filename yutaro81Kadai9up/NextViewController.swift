3a7d166afd34331f17fec14fac17f2ea6e484e4b
yutaro <89335899+yuruhuwakumiai@users.noreply.github.com>
2021-11-13 11:29:00 +0900

    課題９再提出

---
+++ yutaro81Kadai9up/NextViewController.swift
@@ -0,0 +1,61 @@
+//
+//  NextViewController.swift
+//  yutaro81Kadai9up
+//
+//  Created by 橋元雄太郎 on 2021/11/12.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    var textlabel: String?
+    @IBAction private func tokyoButton(_ sender: Any) {
+        textlabel = "東京都"
+    }
+
+    @IBAction private func kanagawaButton(_ sender: Any) {
+        textlabel = "神奈川県"
+    }
+
+    @IBAction private func saitamaButton(_ sender: Any) {
+        textlabel = "埼玉県"
+    }
+
+    @IBAction private func chibaButton(_ sender: Any) {
+        textlabel = "千葉県"
+    }
+}
+// 2021.11.12  修正考案1
+//    import UIKit
+//
+//    class NextViewController: UIViewController {
+//    @IBOutlet weak private var button1: UIButton!
+//    @IBOutlet weak private var button2: UIButton!
+//    @IBOutlet weak private var button3: UIButton!
+//    @IBOutlet weak private var button4: UIButton!
+//
+//        var inputText: String?
+//    private var timingData: Int = 0
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+// for文の中でキャストを「！」を使わないで行う方法 をどうするか swftlint警告
+//        for num in 1...4 {
+//            (value(forKey: "button\(num)") as! UIButton).tag = num
+//        }
+//    }
+
+//    @IBAction private func placeButton(_ sender: UIButton) {
+//        timingData = sender.tag
+//        switch timingData {
+//        case 1:
+//            inputText = "東京都"
+//        case 2:
+//            inputText = "神奈川県"
+//        case 3:
+//            inputText = "埼玉県"
+//        case 4:
+//            inputText = "千葉県"
+//        default:
+//            break
+//        }
+//    }
---
+++ yutaro81Kadai9up/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yutaro81Kadai9up
+//
+//  Created by 橋元雄太郎 on 2021/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak private var resultLabel: UILabel!
+
+    @IBAction private func exitDone(segue: UIStoryboardSegue) {
+        let nextVC = segue.source as? NextViewController
+        resultLabel.text = nextVC?.textlabel
+    }
+
+    @IBAction private func exit(segue: UIStoryboardSegue) {
+    }
+}
